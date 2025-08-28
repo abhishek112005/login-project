55133fc6fdba28db6ea93685fd41a68366d4bacf
abhishek112005 <abhishekpothanagari@gmail.com>
2025-08-28 08:31:02 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,60 @@
 import { useState } from 'react'
 import './App.css'
-
+function App() {
+  // useStates
+// email-->var stores val
+// setEmail--> used for setting and clearing the email var value
+const [email,setEmail]=useState("");
+const [password,setPassword]=useState("");
 const handleLogin=(e)=>{
   e.preventDefault();
   console.log("btn-clicked");
-  window.location.reload();
+  if(email && password){
+  console.log("Entered email is: ",email);
+  console.log("Entered password is: ",password);
+  alert("Details sent to console")
+  }else{
+    alert("please enter mail and password!")
+  }
+
+  // clear the varaibales
+  setEmail("");
+  setPassword("");
+  // window.location.reload();
   
 }
-function App() {
   return (
    <div className='flex items-center justify-center h-screen bg-gray-900'>
     <div className='bg-white rounded-lg p-8 w-96'>
       <h2
       className='text-2xl text-center font-bold text-gray-800 mb-6'
       >Login</h2>
-      <form action="">
+      <form action="" onSubmit={handleLogin}>
         {/* email div */}
         <div>
           <label className='block '>Email</label>
           <input type="email"  placeholder="Enter your email"
           className=' w-full px-4 py-2 border rounded-lg'
+          value={email}
+          onChange={(e)=>{
+            setEmail(e.target.value)
+          }}
           />
         </div>
         {/* password */}
         <div>
           <label className='block mt-4'>Password</label>
           <input type="password" placeholder='enter your password' 
+          value={password}
+          onChange={(e)=>{
+            setPassword(e.target.value)
+          }}
           className=' w-full px-4 py-2 border rounded-lg mb-4'
           />
         </div>
         {/* btn  */}
         <button className='w-58 bg-gray-500 text-white py-2 rounded-lg ml-10 mt-4'
-        type="submit" onClick={handleLogin}
+        type="submit"
         >
           Login
         </button>
